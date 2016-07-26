6a73ced9b8f2ff38e47bf3ea6f80edcc5176ea6b
Phonicavi <mardpazin7san@hotmail.com>
2016-07-26 17:33:54 +0800

    first push

---
+++ README.md
@@ -0,0 +1,10 @@
+# Chrome-extensions.Merriam-Webster  
+
+> I love GRE  I love GRE  I love GRE :)  
+
+	MW这个辞典查出来有simple-def和full-def两套 我已经拿出来丢到两个数组里了  
+	注意如果一个词有多个词性的话每个词性都有一套simple & full  
+	实在是懒得排版了 目前就全丢到popup窗口里的 你们谁心情好搞一下 我谢谢你  
+
+> 我爱GRE  我爱GRE  我爱GRE :)  
+
---
+++ search.js
@@ -0,0 +1,36 @@
+var request;
+var vocabulary;
+
+function setChildTextNode(elementId, text) {
+  if (document.getElementById(elementId) != null) {
+    document.getElementById(elementId).innerText = text;
+  }
+}
+
+var sim_def, full_def;
+
+function processResponse(responseText) {
+  // tense-box quick-def-box simple-def-box card-box def-text 
+  // card-box full-def-box secondary-card def-text 
+  document.getElementById('content').innerHTML = responseText;
+  sim_def = document.getElementsByClassName('tense-box quick-def-box simple-def-box card-box def-text ');
+  full_def = document.getElementsByClassName('card-box full-def-box secondary-card def-text ');
+  // document.getElementById('content').innerHTML = sim_def[0].innerHTML.toString();
+  // document.getElementById('content').innerHTML = full_def[0].innerHTML.toString();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('#button').addEventListener('click', function() {
+    vocabulary = document.getElementById('vocabulary').value;
+    request = new XMLHttpRequest();
+    request.open("GET", "http://www.merriam-webster.com/dictionary?s="+vocabulary, true);
+    request.send();
+    request.onreadystatechange = function() {
+      if (request.readyState == 4 && request.status == 200) {
+        processResponse(request.responseText);
+      } else {
+        setChildTextNode('content', "loading...");
+      }
+    }
+  });
+});
